69272b2f27a56d6757dc44682ae264a6ca3d54e9
Aryaman Shrivastava <63368155+RussianFlipFlop@users.noreply.github.com>
2023-10-21 19:19:03 -0700

    Create scrollhandler-simple.js

---
+++ scrollhandler-simple.js
@@ -0,0 +1,55 @@
+$("#lsn1-b").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".lesson1-s").offset().top + $(".lesson1-s").height() / 2 - $(window).height() / 2},
+        'slow');
+});
+$("#lsn2-b").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".lesson2-s").offset().top + $(".lesson2-s").height() / 2 - $(window).height() / 2},
+        'slow');
+});
+$("#prct1-b").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".practice1-s").offset().top + $(".practice1-s").height() / 2 - $(window).height() / 2},
+        'slow');
+});
+$("#lsn3-b").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".lesson3-s").offset().top + $(".lesson3-s").height() / 2 - $(window).height() / 2},
+        'slow');
+});
+$("#lsn4-b").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".lesson4-s").offset().top + $(".lesson4-s").height() / 2 - $(window).height() / 2},
+        'slow');
+});
+$("#prct2-b").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".practice2-s").offset().top + $(".practice2-s").height() / 2 - $(window).height() / 2},
+        'slow');
+});
+$("#lsn5-b").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".lesson5-s").offset().top + $(".lesson5-s").height() / 2 - $(window).height() / 2},
+        'slow');
+});
+$("#lsn6-b").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".lesson6-s").offset().top + $(".lesson6-s").height() / 2 - $(window).height() / 2},
+        'slow');
+});
+$("#prct3-b").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".practice3-s").offset().top + $(".practice3-s").height() / 2 - $(window).height() / 2},
+        'slow');
+});
+$("#lsn7-b").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".lesson7-s").offset().top + $(".lesson7-s").height() / 2 - $(window).height() / 2},
+        'slow');
+});
+$("#lsn8-b").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".lesson8-s").offset().top + $(".lesson8-s").height() / 2 - $(window).height() / 2},
+        'slow');
+});
